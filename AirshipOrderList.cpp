c70250ff9837c3ccc451a69841440d8302baf340
llyons151 <lukelyons309@gmail.com>
2025-11-10 21:02:58 +0000

    added removal function

--- AirshipOrderList.cpp
+++ AirshipOrderList.cpp
@@ -25,18 +25,11 @@ void AirshipOrderList::addDelivery(std::string szCust, std::string szItm, int iQ
     };
 
     Delivery* current = pHead;
-    while(current != nullptr)
+    while(current->pNext != nullptr)
     {
-        if(current->pNext == nullptr)
-        {
-            current->pNext = newDelivery;
-            return;
-        }
-        else
-        {
-            current = current->pNext;
-        }
+        current = current->pNext;
     };
+    current->pNext = newDelivery;
 };
 
 Delivery* AirshipOrderList::findDelivery(std::string szCust, std::string szItm) const
@@ -49,4 +42,34 @@ Delivery* AirshipOrderList::findDelivery(std::string szCust, std::string szItm) 
         current = current->pNext;
     };
     return nullptr;
+};
+
+
+bool AirshipOrderList::removeDelivery(string szCust, string szItm)
+{
+    if(pHead == nullptr) return false;
+
+    if(pHead->szName == szCust && pHead->szItem == szItm)
+    {
+        Delivery* temp = pHead;
+        pHead = pHead->pNext;
+        delete temp;
+        return true;
+    };
+
+    Delivery* prev = pHead;
+    Delivery* current = pHead->pNext;
+
+    while(current != nullptr)
+    {
+        if(current->szName == szCust && current->szItem == szItm)
+        {
+            prev->pNext = current->pNext;
+            delete current;
+            return true;
+        };
+        prev = current;
+        current = current->pNext;
+    };
+    return false;
 };
